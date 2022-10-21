3f00f70dc0179115745e9165e4bfdcdeb3451421
JWB <john.skem9@gmail.com>
2022-10-20 21:44:21 -0400

    Update modals.js

--- resources/js/modals.js
+++ resources/js/modals.js
@@ -1,30 +1,13 @@
+let modalsElement = document.getElementById('laravel-livewire-modals');
 
-// var Modal = $.fn.modal;
-// let modalsElement = document.getElementById('laravel-livewire-modals');
-
-if ($.fn.modal == undefined) {
-    console.error("Chưa có thư viện Modal.js hoặc bootstrap chưa hỗ trợ");
-}
-
-// modalsElement.addEventListener('hidden.bs.modal', () => {
-//     Livewire.emit('resetModal');
-// });
-
-$('#laravel-livewire-modals').on('hidden.bs.modal', function (e) {
-    Livewire.emit('resetModal');
+$(modalsElement).on('hidden.bs.modal', () => {
+  Livewire.emit('resetModal');
 });
 
 Livewire.on('showBootstrapModal', () => {
-    // let modal = $.fn.modal.getInstance(modalsElement);
-    // if (!modal) {
-    //     modal = new Modal(modalsElement);
-    // }
-    // modal.show();
-    $('#laravel-livewire-modals').modal('show');
+  $(modalsElement).modal('show');
 });
 
 Livewire.on('hideModal', () => {
-    // let modal = Modal.getInstance(modalsElement);
-    // modal.hide();
-    $('#laravel-livewire-modals').modal('hide');
+  $(modalsElement).modal('hide');
 });
